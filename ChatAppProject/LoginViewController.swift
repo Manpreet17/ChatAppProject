34b24737affe745a515cfee1c3e6428bac640bb1
Manpreet Dhillon <dhillonmanpreet17@gmail.com>
2018-11-11 10:07:43 -0600

    adding initial commit

---
+++ ChatAppProject/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  ChatAppProject
+//
+//  Created by Manpreet Dhillon on 2018-11-09.
+//  Copyright © 2018 UofR. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Setting login button's  border and color
+        loginButton.layer.borderWidth = 1.5;
+        loginButton.layer.cornerRadius = 21.0;
+        loginButton.layer.borderColor = UIColor(red: 216/255, green: 216/255, blue: 216/255, alpha: 1.0).cgColor;
+        
+    }
+
+
+}
+
---
+++ ChatAppProject/RegisterViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RegisterViewController.swift
+//  ChatAppProject
+//
+//  Created by Manpreet Dhillon on 2018-11-10.
+//  Copyright © 2018 UofR. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var registerButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Setting register button border and color
+        registerButton.layer.borderWidth = 1.5;
+        registerButton.layer.cornerRadius = 21.0;
+        registerButton.layer.borderColor = UIColor(red: 216/255, green: 216/255, blue: 216/255, alpha: 1.0).cgColor;
+    
+    }
+
+}
