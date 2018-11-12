5299ae36bceb8e2fa51fa8f53465c632b12c40d2
Mehak Luthra <mehakluthra@mehaks-mbp>
2018-11-11 22:57:08 -0600

    some initial changes

--- ChatAppProject/LoginViewController.swift
+++ ChatAppProject/LoginViewController.swift
@@ -13,12 +13,16 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Setting login button's  border and color
+        setUIView()
+    }
+    
+    // MARK: Private Functions
+    private func setUIView()
+    {
+        //Setting login button's border and color
         loginButton.layer.borderWidth = 1.5;
         loginButton.layer.cornerRadius = 21.0;
         loginButton.layer.borderColor = UIColor(red: 216/255, green: 216/255, blue: 216/255, alpha: 1.0).cgColor;
-        
     }
 
 
--- ChatAppProject/RegisterViewController.swift
+++ ChatAppProject/RegisterViewController.swift
@@ -13,12 +13,16 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var registerButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUIView()
+    }
+    
+    // MARK: Private Functions
+    private func setUIView()
+    {
         //Setting register button border and color
         registerButton.layer.borderWidth = 1.5;
         registerButton.layer.cornerRadius = 21.0;
         registerButton.layer.borderColor = UIColor(red: 216/255, green: 216/255, blue: 216/255, alpha: 1.0).cgColor;
-    
     }
 
 }
